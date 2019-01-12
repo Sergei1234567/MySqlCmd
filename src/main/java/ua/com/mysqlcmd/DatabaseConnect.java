07624ff4a86d1495f4c9f90a147729f062d864f5
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2019-01-12 14:59:11 +0300

    refact pom.xml

--- src/main/java/ua/com/mysqlcmd/DatabaseConnect.java
+++ src/main/java/ua/com/mysqlcmd/DatabaseConnect.java
@@ -3,7 +3,6 @@ package ua.com.mysqlcmd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class DatabaseConnect {
