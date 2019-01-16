da916896a0626ccc9998a9dec2d49d7b5fc62cc2
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2019-01-16 12:45:13 +0300

    переход к ооп

---
+++ src/main/java/MainController.java
@@ -0,0 +1,27 @@
+import ua.com.mysqlcmd.DatabaseConnect;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class MainController {
+    public static void main(String[] args) throws SQLException{
+        DatabaseConnect manager = new DatabaseConnect();
+        System.out.println("Hi user!");
+        System.out.println("Please enter the database name, username, password in the format:" +
+                "connect databaseName userName  password\n__________________");
+        Scanner scan = new Scanner(System.in);
+        String comand = scan.nextLine();
+        String[] data = comand.split("\\s");
+        String connectUser = data[0];
+        String databaseName = data[1];
+        String userName = data[2];
+        String password = data[3];
+        System.out.println(connectUser + ":" + "Connection successful!");
+
+        manager.connection(databaseName, userName, password);
+        Connection connection = manager.getConnection();
+        connection.close();
+    }
+
+}
--- src/main/java/ua/com/mysqlcmd/DatabaseConnect.java
+++ src/main/java/ua/com/mysqlcmd/DatabaseConnect.java
@@ -3,31 +3,11 @@ package ua.com.mysqlcmd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class DatabaseConnect {
     private Connection connection;
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        DatabaseConnect manager = new DatabaseConnect();
-        System.out.println("Hi user!");
-        System.out.println("Please enter the database name, username, password in the format:" +
-                "connect databaseName userName  password\n__________________");
-        Scanner scan = new Scanner(System.in);
-        String comand = scan.nextLine();
-        String[] data = comand.split("\\s");
-        String connectUser = data[0];
-        String databaseName = data[1];
-        String userName = data[2];
-        String password = data[3];
-        System.out.println(connectUser + ":" + "Connection successful!");
-
-        manager.connection(databaseName, userName, password);
-        Connection connection = manager.getConnection();
-        connection.close();
-    }
-
-    private void connection(String databaseName, String userName, String password) {
+    public void connection(String databaseName, String userName, String password) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -42,7 +22,9 @@ public class DatabaseConnect {
         }
     }
 
-    private Connection getConnection() {
+    public Connection getConnection() {
         return connection;
     }
 }
+
+
