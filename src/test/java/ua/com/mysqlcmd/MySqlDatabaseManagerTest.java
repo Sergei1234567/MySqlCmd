c9adc9d4020720da485da3c99c3c334c06f4c98a
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2019-01-21 17:57:29 +0300

    правка тест методов

--- src/test/java/ua/com/mysqlcmd/MySqlDatabaseManagerTest.java
+++ src/test/java/ua/com/mysqlcmd/MySqlDatabaseManagerTest.java
@@ -4,11 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import ua.com.mysqlcmd.view.manager.MySqlDatabaseManager;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import static junit.framework.TestCase.assertEquals;
 
 public class MySqlDatabaseManagerTest {
     private MySqlDatabaseManager manager;
@@ -16,12 +13,10 @@ public class MySqlDatabaseManagerTest {
     @Before
     public void setup() {
         manager = new MySqlDatabaseManager();
-        manager.connect("sqlcmd", "root", "root");
     }
 
     @Test
     public void testConnect() throws RuntimeException{
-        Connection connection;
 
         String databaseName = "sqlcmd";
         String userName = "root";
@@ -33,7 +28,7 @@ public class MySqlDatabaseManagerTest {
             throw new RuntimeException("Please add jdbc jar to project.", e);
         }
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, userName, password);
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, userName, password);
         } catch (SQLException e) {
             throw new RuntimeException(String.format("Cant get connection for database:%s user:%s,",
                     databaseName, userName),
