8a89260f5e06eafa133a5901cd7f557a1344ab29
Berezhnoi Sergei <berezhnoi.serg@gmail.com>
2019-02-24 13:57:09 +0300

    правка названий тест методов

--- src/test/java/ua/com/mysqlcmd/MySqlDatabaseManagerTest.java
+++ src/test/java/ua/com/mysqlcmd/MySqlDatabaseManagerTest.java
@@ -5,7 +5,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import ua.com.mysqlcmd.view.manager.MySqlDatabaseManager;
+
 import java.util.Set;
+
 import static junit.framework.TestCase.assertTrue;
 
 
@@ -22,28 +24,36 @@ public class MySqlDatabaseManagerTest {
 
 
     @Test
-    public void testConnect() {
+    public void connect_ShouldThrowsRuntimeException_WhenInvalidPassword() {
+        //Given - Then
         exception.expect(RuntimeException.class);
         exception.expectMessage("Could not get database connection\n:databaseName:sqlcmd user:root password:roo,");
+        //When
         manager.connect("sqlcmd", "root", "roo");
     }
 
     @Test
-    public void testGetTableName() {
+    public void shouldContainTableNames_WhenConnectionSuccessful() {
+        //Given
         manager.connect("sqlcmd", "root", "root");
+        //When
         Set<String> tables = manager.getTableNames();
+        //Then
         assertTrue(tables.contains("user"));
         assertTrue(tables.contains("test"));
     }
 
     @Test
-    public void testGetTableName2() {
+    public void shouldContainTableNames_WhenTablesNotEqualToZero() {
+        //Given
         manager.connect("sqlcmd", "root", "root");
+        //When
         Set<String> tables = manager.getTableNames();
+        //Then
         if (tables != null) {
             assertTrue(tables.contains("test"));
             assertTrue(tables.contains("user"));
-        }else {
+        } else {
             assertTrue(tables.contains(""));
         }
     }
