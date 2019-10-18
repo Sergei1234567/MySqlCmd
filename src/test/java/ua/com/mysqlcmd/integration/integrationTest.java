055d8304cc8b826967a3d5eb7cc7c75888013a75
Berezhnoi Sergei <berezhnoi.serg@gmail.com>
2019-10-18 15:07:12 +0300

    написал интеграционный тест для приложения

--- src/test/java/ua/com/mysqlcmd/integration/integrationTest.java
+++ src/test/java/ua/com/mysqlcmd/integration/integrationTest.java
@@ -29,7 +29,6 @@ public class integrationTest {
         System.setOut(new PrintStream(out));
     }
 
-
     @Test
     public void testHelp() {
 
@@ -84,6 +83,166 @@ public class integrationTest {
         //then
         assertEquals("Hello user!\r\n" +
                 "Please enter a command in the format: connect databaseName userName password\r\n" +
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testGetTableNames_WithoutConnect() {
+
+        //given
+        in.add("tables");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                "You conneot use the command 'tables' yet do not connect using the command connect databaseName userName password\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testDisplayingTableContent_WithoutConnect() {
+
+        //given
+        in.add("test");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                "You conneot use the command 'test' yet do not connect using the command connect databaseName userName password\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testUnsupported() {
+
+        //given
+        in.add("unsupported");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                "You conneot use the command 'unsupported' yet do not connect using the command connect databaseName userName password\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testUnsupportedAfterConnect() {
+
+        //given
+        in.add("connect sqlcmd root root");
+        in.add("unsupported");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                //connect
+                "Success\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //unsupported
+                "Failure due:no data in the table\r\n" +
+                "Try again\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //exit
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testGetTableNames_AfterConnect() {
+
+        //given
+        in.add("connect sqlcmd root root");
+        in.add("tables");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                //connect
+                "Success\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //getTableNames
+                "[[test, user]]\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //exit
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testDisplayingTableContent_WithError_AfterConnect() {
+
+        //given
+        in.add("connect sqlcmd root root");
+        in.add("tableName");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                //connect
+                "Success\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //getTableNames
+                "Failure due:no data in the table\r\n" +
+                "Try again\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //exit
+                "See you soon!\r\n", getData());
+    }
+
+    @Test
+    public void testConnect_AfterConnect() {
+
+        //given
+        in.add("connect sqlcmd root root");
+        in.add("tables");
+        in.add("connect world root root");
+        in.add("tables");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Hello user!\r\n" +
+                "Please enter a command in the format: connect databaseName userName password\r\n" +
+                //connect sqlcmd
+                "Success\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //tables
+                "[[test, user]]\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //connect world
+                "Success\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //tables
+                "[[country, city, countrylanguage]]\r\n" +
+                "Enter the command (or help for help)\r\n" +
+                //exit
                 "See you soon!\r\n", getData());
     }
 }
